ae406e19f85cc17a17d73887177ac2b394e367d2
Thiago Bignotto <bignotto@gmail.com>
2023-10-01 06:06:22 -0300

    configured chakra ui and fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/components/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { theme } from "../styles/theme";
+
+import { Pacifico, Roboto } from "next/font/google";
+
+const textFont = Roboto({ subsets: ["latin"], weight: "400" });
+const headingFont = Roboto({ subsets: ["latin"], weight: "700" });
+const blackFont = Roboto({ subsets: ["latin"], weight: "900" });
+const logoFont = Pacifico({ subsets: ["latin"], weight: "400" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <style jsx global>
+                {`
+                    :root {
+                        --font-textFont: ${textFont.style.fontFamily};
+                        --font-headingFont: ${headingFont.style.fontFamily};
+                        --font-blackFont: ${blackFont.style.fontFamily};
+                        --font-logoFont: ${logoFont.style.fontFamily};
+                    }
+                `}
+            </style>
+            <ChakraProvider theme={theme}>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </>
+    );
 }
